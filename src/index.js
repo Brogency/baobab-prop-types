0e02f8a9bf79ef6f149e2baf99427c608bc5a13f
Vadim Laletin <vadim@bro.agency>
2016-12-06 12:03:00 +0700

    Fix required and cursor/baobab prop types

--- src/index.js
+++ src/index.js
@@ -3,31 +3,35 @@ import Baobab from 'baobab';
 const PropTypes = {};
 
 function errorMessage(propName, what) {
-  return 'prop type `' + propName + '` is invalid; it must be ' + what + '.';
+    return 'prop type `' + propName + '` is invalid; it must be ' + what + '.';
 }
 
 function isRequired(props, propName) {
     if (props[propName] == null) {
-      return new Error('prop type `' + propName + '` is a required.');
+        return new Error('prop type `' + propName + '` is a required.');
     }
 }
 
-PropTypes.baobab = function(props, propName) {
-  if (!(propName in props))
-    return;
+PropTypes.baobab = function (props, propName) {
+    if (props[propName] == null) {
+        return;
+    }
 
-  if (!(props[propName] instanceof Baobab))
-    return new Error(errorMessage(propName, 'a Baobab tree'));
+    if (!(props[propName] instanceof Baobab)) {
+        return new Error(errorMessage(propName, 'a Baobab tree'));
+    }
 };
 
 PropTypes.baobab.isRequired = isRequired;
 
-PropTypes.cursor = function(props, propName) {
-  if (!(propName in props))
-    return;
+PropTypes.cursor = function (props, propName) {
+    if (props[propName] == null) {
+        return;
+    }
 
-  if (!(props[propName] instanceof Baobab.Cursor))
-    return new Error(errorMessage(propName, 'a Baobab.Cursor'));
+    if (!(props[propName] instanceof Baobab.Cursor)) {
+        return new Error(errorMessage(propName, 'a Baobab.Cursor'));
+    }
 };
 
 PropTypes.cursor.isRequired = isRequired;
